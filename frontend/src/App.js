37bd43dc6a4845a75a1dd2ffc01b6fa5bbbda6b1
ctsTech2 <tempjuice@hotmail.com>
2024-06-20 09:35:05 -0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, MUI!</h1>
+      <Button variant="contained" color="primary">
+        My MUI Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
+
